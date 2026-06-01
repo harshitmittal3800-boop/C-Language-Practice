6bece44b81cc12c01bce43b2fd1a382305300f59
harshitmittal3800-boop <harshitmittal3800@gmail.com>
2026-06-01 17:43:48 +0530

    Implement basic calculator functionality in C
    A basic calculator developed in C language using switch-case. It performs addition, subtraction, multiplication, and division operations based on user input.

---
+++ Calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   float a,b;
+   char op;
+   printf("Enter two number");
+   scanf("%f %f",&a, &b);
+   
+   printf("Enter operator (+,-,*,/,%):");
+   scanf(" %c",&op);
+   
+   switch(op){
+       case '+':printf("Result = %.2f",a+b);
+                break;
+       case '-':printf("Result = %.2f",a-b);
+                break;
+       case '*': printf("Result = %.2f", a*b);
+                break;
+       case '/':printf("Result = %.2f",a/b);
+                break;
+       default:printf("Invalid Operator");
+                break;
+   }
+    return 0;
+}
